aed363b04facb75f9f37a3525a43a9c642f47592
shakarrathod9632 <sakkurathod8585@gmail.com>
2025-11-04 21:32:33 +0530

    Fix allowed hosts for Render

--- feemanagement/settings.py
+++ feemanagement/settings.py
@@ -20,7 +20,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 # DEBUG = True
 
 #  Allowed Hosts
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 
